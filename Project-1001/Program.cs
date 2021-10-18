3309de368ddcbdd30b25e45f93c3940081ca8958
Nischal Joshi <60282678+Nischal-012@users.noreply.github.com>
2021-10-18 07:43:23 +0545

    Add files via upload

---
+++ Project-1001/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Project_1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any two number");
+            double a = Convert.ToDouble(Console.ReadLine()), b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("1.Addition\n"+
+                            "2.Subtraction\n"+
+                            "3.Multiplication\n"+
+                            "4.Division");
+            int test = Convert.ToInt32(Console.ReadLine());
+            if (test == 1)
+                Console.WriteLine(a + b);
+
+            if (test == 2)
+            { Console.WriteLine(a - b); 
+            }
+                
+            if (test == 3)
+                {
+                Console.WriteLine(a * b); 
+                }
+
+            if (test == 4)
+            {
+                Console.WriteLine(a / b);
+            }
+            Console.ReadLine();
+        }
+    }
+}
